666a77a6dfeccd31964881e33a34a8d33782b364
Vaibhav Sabhani <vvsabhani1@gmail.com>
2026-05-29 09:39:05 +0530

    console the mongo connection

--- EM_Backend/server.js
+++ EM_Backend/server.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
