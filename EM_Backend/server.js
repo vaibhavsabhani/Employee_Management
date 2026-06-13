a465841f545d7dbe146bfee544553f8d6b45ba04
vaibhav sabhani <vaibhavsabhani.prolix@gmail.com>
2026-06-13 17:57:48 +0530

    remove debugging code

--- EM_Backend/server.js
+++ EM_Backend/server.js
@@ -26,7 +26,7 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("MongoDB Connected");
-  })
+  })               
   .catch((err) => {
     console.log(err);
   });
@@ -55,26 +55,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-app.get("/test-env", (req, res) => {
-  res.json({
-    mongoExists: !!process.env.MONGO_URL,
-    mongoUriLength: process.env.MONGO_URL?.length || 0,
-    JWT_SECRET : process.env.JWT_SECRET,
-    PORT:process.env.PORT
-  });
-});
-
-app.get("/api/test-mongo", async (req, res) => {
-  try {
-    const state = mongoose.connection.readyState;
-
-    return res.json({
-      readyState: state,
-      mongoUriExists: !!process.env.MONGO_URL,
-    });
-  } catch (err) {
-    return res.json(err);
-  }
 });
